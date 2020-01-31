7a3db393e9154cfb49edfb6a620857868e93dea5
Marco Bellaccini <24960638+marcobellaccini@users.noreply.github.com>
2020-01-31 01:47:27 +0100

    Encrypted class

--- spec/nanvault_spec.cr
+++ spec/nanvault_spec.cr
@@ -3,7 +3,14 @@ require "./spec_helper"
 describe Nanvault do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  describe Nanvault::Encrypted do
+    describe "#initialize" do
+      it "correctly loads header and body" do
+        enc = Nanvault::Encrypted.new ["HEADER", "BODY1", "BODY2"]
+        enc.header.should eq("HEADER")
+        enc.body.should eq("BODY1BODY2")
+      end
+    end
   end
+
 end
--- src/nanvault.cr
+++ src/nanvault.cr
@@ -3,4 +3,14 @@ module Nanvault
   VERSION = "0.1.0"
 
   # TODO: Put your code here
+
+  # TODO: Write documentation
+  class Encrypted
+    property header : String, body : String
+
+    def initialize(@ctext_lines : Array(String))
+      @header = ctext_lines[0]
+      @body = ctext_lines[1..-1].join()
+    end
+  end
 end
